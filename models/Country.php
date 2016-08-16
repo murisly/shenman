38d07b3ad8a230efc5bde93e4a08f4580d2bc6ee
murisly <murisly2015@gmail.com>
2016-08-16 19:59:30 +0800

    database test

--- models/Country.php
+++ models/Country.php
@@ -1,16 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: macalyou
- * Date: 2016/7/12
- * Time: 23:12
- */
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
+use Yii;
 
-class Country extends ActiveRecord
+/**
+ * This is the model class for table "st_country".
+ *
+ * @property integer $id
+ * @property string $code
+ * @property string $name
+ * @property integer $population
+ */
+class Country extends \yii\db\ActiveRecord
 {
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%country}}';
+    }
 
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['population'], 'integer'],
+            [['code', 'name'], 'string', 'max' => 10],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'code' => 'Code',
+            'name' => 'Name',
+            'population' => 'Population',
+        ];
+    }
 }
